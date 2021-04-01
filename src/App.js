819cb150fa962260d3f289bc3145cfd8530061c7
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-04-01 17:58:21 +0500

    Refactoring: delete node-sass from project & refactoring SCSS to CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.appWrapper}>
+      <div className={classes.headerContainer}>
+          <div className={classes.header}>
+              Header
+          </div>
+      </div>
+      <div className={classes.mainContainer}>
+          <div className={classes.sidebar}>
+              <nav className={classes.nav}>
+                  <a href="#"><span className={classes.navIcon + ' ' + "material-icons"}>person_outline</span>Profile</a>
+                  <a href="#" className={classes.active}><span className={classes.navIcon + ' ' + "material-icons"}>people_outline</span>Users</a>
+                  <a href="#"><span className={classes.navIcon + ' ' + "material-icons"}>mail</span>dialogs</a>
+              </nav>
+          </div>
+          <div className={classes.container}>
+              container
+          </div>
+      </div>
+      <div className={classes.footerContainer}>
+          <div className={classes.footer}>
+              Footer
+          </div>
+      </div>
     </div>
   );
 }
